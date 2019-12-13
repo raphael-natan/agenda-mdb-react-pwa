c9e3d5c8d9fd8fef0295abad1642ec2f872d2e2c
Raphael Natan Nunes <raphaelnatanpt@gmail.com>
2019-12-13 00:12:20 -0300

    add events and remove

--- src/index.js
+++ src/index.js
@@ -3,36 +3,50 @@ import ReactDOM from 'react-dom'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbreact/dist/css/mdb.css'
-import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from 'mdbreact'
+import {
+    MDBBtn, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol, MDBModal, MDBModalBody, MDBModalHeader,
+    MDBModalFooter, MDBInput
+} from 'mdbreact'
 import './index.css'
 
-function App() {
-    const [events, setEvents] = useState(){
-        id: 1,
-        time: "10:00",
-        title: "Breakfast with Simon",
-        location: "Lounge Caffe",
-        description: "Discuss Q3 targets"
-    },
+const App = () => {
+    const [events, setEvents] = useState([
+        {
+            id: 1,
+            time: "10:00",
+            title: "Breakfast with Simon",
+            location: "Lounge Caffe",
+            description: "Discuss Q3 targets"
+        },
         {
             id: 2,
             time: "10:30",
             title: "Daily Standup Meeting (recurring)",
             location: "Warsaw Spire Office"
         },
-        { id: 3, time: "11:00", title: "Call with HRs" },
+        {
+            id: 3,
+            time: "11:00",
+            title: "Call with HRs"
+        },
         {
             id: 4,
             time: "11:00",
             title: "Lunch with Timothy",
             location: "Canteen",
             description: "Project evaluation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a"
-        })
+        }
+    ])
+    const [modal, setModal]=useState(false)
 
-    /* handleDelete = eventId => {
-        const eventsNew = events.filter(e => e.id !== eventId)
-        setEvents({ eventsNew })
-    } */
+    const handleDelete = eID => {
+        const eventsNew = events.filter(e => e.id !== eID)
+        setEvents(eventsNew)
+    }
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
 
     return (
         <React.Fragment>
@@ -49,13 +63,13 @@ function App() {
                                     title={event.title}
                                     location={event.location}
                                     description={event.description}
-                                    /* onDelete={()=>{handleDelete}} */
+                                    onDelete={handleDelete}
                                 />
                             ))}
                         </div>
                         <MDBRow className="mb-4">
                             <MDBCol xl="3" md="6" className="mx-auto text-center">
-                                <MDBBtn color="info" rounded>
+                                <MDBBtn color="info" rounded onClick={toggleModal}>
                                     Add Event
                                 </MDBBtn>
                             </MDBCol>
@@ -89,11 +103,28 @@ function App() {
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
+            {/* Modal code below: */}
+            <MDBModal isOpen={modal} /* NÃO FUNCIONA DEVIDO A TAG SER CLASS???*** toggle={toggleModal} */>
+                <MDBModalHeader
+                    className="text-center"
+                    titleClass="w-100 font-weight-bold"
+                    toggle={toggleModal}
+                >
+                    Add new event
+                </MDBModalHeader>
+                <MDBModalBody>
+                    Body
+                </MDBModalBody>
+                <MDBModalFooter className="justify-content-center">
+                    
+                </MDBModalFooter>
+            </MDBModal>
         </React.Fragment>
     )
 }
 
-function Event(props) {
+const Event = props => {
+    const [id, setId] = useState(props.id)
     const [time, setTime] = useState(props.time)
     const [title, setTitle] = useState(props.title)
     const [location, setLocation] = useState(props.location)
@@ -104,7 +135,7 @@ function Event(props) {
             <div className="media mt-1">
                 <h3 className="h3-responsive font-weight-bold mr-3">{time}</h3>
                 <div className="media-body mb-3 mb-lg-3">
-                    <MDBBadge color="danger" className="ml-2 float-right" >-</MDBBadge>
+                    <MDBBadge color="danger" className="ml-2 float-right" onClick={() => props.onDelete(id)}>-</MDBBadge>
                     <h6 className="mt-0 font-weight-bold">{title}</h6>{" "}
                     <hr className="hr-bold my-2" />
                     {location && (
