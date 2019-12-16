b789d6150e44f9bd41982742031e84b706f464e3
raphael <raphaelnatanpt@gmail.com>
2019-12-16 00:58:55 -0300

    add events

--- src/index.js
+++ src/index.js
@@ -37,7 +37,8 @@ const App = () => {
             description: "Project evaluation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a"
         }
     ])
-    const [modal, setModal]=useState(false)
+    const [modal, setModal] = useState(false)
+    const [eventsTemp, setEventsTemp] = useState()
 
     const handleDelete = eID => {
         const eventsNew = events.filter(e => e.id !== eID)
@@ -46,6 +47,45 @@ const App = () => {
 
     const toggleModal = () => {
         setModal(!modal)
+    }
+
+    const handleInputChange = inputName => value => {
+        const nextValue = value
+        setEventsTemp({...eventsTemp,
+            [inputName]: nextValue
+          })
+        console.log("setei eventsTemp no input")
+        console.log(eventsTemp)
+    }
+
+    const addEvent = () => {
+        let newArray = {
+            id: events.length ? events[events.length - 1].id + 1 : 1,
+            time: eventsTemp.time,
+            title: eventsTemp.title,
+            location: eventsTemp.location,
+            description: eventsTemp.description,
+            value: events.length > 5 ? "Its's grater then 5" : "Its lower or equal 5"
+        }
+        console.log('let new array')
+        console.log(newArray)
+
+        console.log('setei new array no temp')
+        setEventsTemp(newArray)
+        console.log(eventsTemp)
+
+        console.log('setei novo events temp no array de events')
+        setEvents([...events, eventsTemp])
+        console.log(events)
+
+        setEventsTemp({
+            time: "",
+            title: "",
+            location: "",
+            description: ""
+        })
+        console.log('limpei events temp')
+        console.log(eventsTemp)
     }
 
     return (
@@ -104,7 +144,7 @@ const App = () => {
                 </MDBRow>
             </MDBContainer>
             {/* Modal code below: */}
-            <MDBModal isOpen={modal} /* NÃO FUNCIONA DEVIDO A TAG SER CLASS???*** toggle={toggleModal} */>
+            <MDBModal isOpen={modal} toggle={toggleModal}> {/*NÃO FUNCIONA DEVIDO A TAG MDB SER CLASS???*** toggle={toggleModal}*/}
                 <MDBModalHeader
                     className="text-center"
                     titleClass="w-100 font-weight-bold"
@@ -113,10 +153,47 @@ const App = () => {
                     Add new event
                 </MDBModalHeader>
                 <MDBModalBody>
-                    Body
+                    <form className="mx-3 grey-text">
+                        <MDBInput
+                            name="time"
+                            label="Time"
+                            icon="clock"
+                            hint="12:30"
+                            group
+                            type="text"
+                            getValue={handleInputChange("time")}
+                        />
+                        <MDBInput
+                            name="title"
+                            label="Title"
+                            icon="edit"
+                            hint="Briefing"
+                            group
+                            type="text"
+                            getValue={handleInputChange("title")}
+                        />
+                        <MDBInput
+                            name="location"
+                            label="Location (optional)"
+                            icon="map"
+                            group
+                            type="text"
+                            getValue={handleInputChange("location")}
+                        />
+                        <MDBInput
+                            name="description"
+                            label="Description (optional)"
+                            icon="sticky-note"
+                            group
+                            type="textarea"
+                            getValue={handleInputChange("description")}
+                        />
+                    </form>
                 </MDBModalBody>
                 <MDBModalFooter className="justify-content-center">
-                    
+                    <MDBBtn color="info" onClick={toggleModal, addEvent}>
+                        Add
+                    </MDBBtn>
                 </MDBModalFooter>
             </MDBModal>
         </React.Fragment>
@@ -124,37 +201,30 @@ const App = () => {
 }
 
 const Event = props => {
-    const [id, setId] = useState(props.id)
-    const [time, setTime] = useState(props.time)
-    const [title, setTitle] = useState(props.title)
-    const [location, setLocation] = useState(props.location)
-    const [description, setDescription] = useState(props.description)
-
     return (
         <React.Fragment>
             <div className="media mt-1">
-                <h3 className="h3-responsive font-weight-bold mr-3">{time}</h3>
+                <h3 className="h3-responsive font-weight-bold mr-3">{props.time}</h3>
                 <div className="media-body mb-3 mb-lg-3">
-                    <MDBBadge color="danger" className="ml-2 float-right" onClick={() => props.onDelete(id)}>-</MDBBadge>
-                    <h6 className="mt-0 font-weight-bold">{title}</h6>{" "}
+                    <MDBBadge color="danger" className="ml-2 float-right" onClick={() => props.onDelete(props.id)}>-</MDBBadge>
+                    <h6 className="mt-0 font-weight-bold">{props.title}</h6>{" "}
                     <hr className="hr-bold my-2" />
-                    {location && (
+                    {props.location && (
                         <React.Fragment>
                             <p className="font-smaller mb-0">
-                                <MDBIcon icon="location-arrow" />{location}
+                                <MDBIcon icon="location-arrow" />{props.location}
                             </p>
                         </React.Fragment>
                     )}
                 </div>
             </div>
-            {description && (
+            {props.description && (
                 <p className="p-2 mb-4  blue-grey lighten-5 blue-grey lighten-5">
-                    {description}
+                    {props.description}
                 </p>
             )}
         </React.Fragment>
     )
 }
-
 
 ReactDOM.render(<><App /></>, document.getElementById("root"))
